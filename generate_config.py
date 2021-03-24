28e23da0ddff57f29b1afe62cb2c2f8b9b67c567
Shen Feiyu <francis_sfy@sjtu.edu.cn>
2021-03-24 20:29:10 +0800

    run many static web in one

--- generate_config.py
+++ generate_config.py
@@ -1,23 +1,21 @@
 import os
 import json
 import argparse
+from typing import List, Dict, Union
 
 
-def main(args: argparse.Namespace):
-    audio_path = args.audio_dir
+def get_test_cases(audio_path: str, cate1: str, cate2: str) -> List[Dict[str, Union[str, Dict]]]:
     cates = [p for p in os.listdir(audio_path) if p[0] != "."]
-    for c in args.cates:
-        assert c in cates
-    cates = args.cates
-    assert len(cates) == 2, "AB test only support two categories"
+    assert cate1 in cates
+    assert cate2 in cates
     files = {}
+    cates = [cate1, cate2]
     for c in cates:
         path = os.path.join(audio_path, c)
         c_files = [f for f in os.listdir(path) if f[0] != "."]
-        files[c] = [os.path.join(path, f) for f in c_files][args.offset: args.end]
-    c1, c2 = files.keys()
-    c1_files = files[c1]
-    c2_files = files[c2]
+        files[c] = [os.path.join(path, f) for f in c_files]
+    c1_files = files[cate1]
+    c2_files = files[cate2]
     assert len(c1_files) == len(c2_files), ""
     ids = list(range(len(c1_files)))
     test_cases = [
@@ -30,6 +28,10 @@ def main(args: argparse.Namespace):
             }
         } for uid, f1, f2 in zip(ids, c1_files, c2_files)
     ]
+    return test_cases
+
+def gen_config_html(cate1: str, cate2: str, audio_path: str, num_per_run: int = 10, base_html_file: str = "index_template.html"):
+    test_cases = get_test_cases(audio_path, cate1, cate2)
     config_dict = {
         "TestName": "IS2021",
         "LoopByDefault": True,
@@ -41,22 +43,35 @@ def main(args: argparse.Namespace):
         "SupervisorContact": "",
         "AudioRoot": "",
         "RandomizeFileOrder": True,
-        "CateMapping": {"A": c1, "B": c2},
+	    "MaxTestsPerRun": num_per_run,
+        "CateMapping": {"A": cate1, "B": cate2},
         "Testsets": test_cases
     }
-
-    with open(args.config_file, "w") as wf:
-        # wf.write(f"// A: {c1} B: {c2} \n")
+    config_name = f"{cate1}_vs_{cate2}.js"
+    with open(f"./config/{config_name}", "w") as wf:
         wf.write("var TestConfig = ")
         wf.write(json.dumps(config_dict, indent=4))
+    html_base = open(base_html_file, "r").read()
+    config_name = f"{cate1}_vs_{cate2}.js"
+    assert "config/configuration.js" in html_base
+    html_base = html_base.replace("config/configuration.js", f"config/{config_name}")
+    html_name = f"{cate1}_vs_{cate2}.html"
+    with open(html_name, "w") as wf:
+        wf.write(html_base)
+    print(f"Generate config: {config_name}, html: {html_name}")
+    
+
+def main(args: argparse.Namespace):
+    base_cate = args.base_cate
+    cmp_cates = args.cmp_cates
+    for cmpc in cmp_cates:
+        gen_config_html(base_cate, cmpc, args.audio_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--audio-dir", type=str, default="./audio/gl")
-    parser.add_argument("--cates", type=str, nargs="+")
-    parser.add_argument("--offset", type=int, default=0)
-    parser.add_argument("--end", type=int, default=10)
-    parser.add_argument("--config-file", type=str, default="./config/pref_test.js")
+    parser.add_argument("--audio-path", type=str, default="./audio/melgan")
+    parser.add_argument("--base-cate", type=str, default="trans_conv_oov50_top10")
+    parser.add_argument("--cmp-cates", type=str, nargs="+", default=["tts3_top10", "tts3_bert_top10", "tts3_glove_top10"])
     args = parser.parse_args()
     main(args)
